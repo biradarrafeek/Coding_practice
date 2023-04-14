1f422fadb49ebf16080b07f783e366e02959acd5
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-04-14 23:52:26 +0530

    Create Greatest2No.java

---
+++ Java/Basic/Greatest2No.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class Greatest2No{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15;
+		int b=10;
+		
+		if(a==b) {
+			System.out.println("Both are equal");
+		}
+		else if(a>b) {
+			System.out.println(a);
+		}
+		else {
+			System.out.println(b);
+		}
+	}
+
+}

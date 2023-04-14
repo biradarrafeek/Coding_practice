1be03b3de27ae88029fadf6a6a8a380744367d30
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-04-15 00:06:48 +0530

    Create StringExample1.java

---
+++ Java/StringBased/StringExample1.java
@@ -0,0 +1,15 @@
+package StringBased;
+
+public class StringExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="Welcome to java Tutorial";
+		String str2="Data Structure and Algorithm";
+		String str3=new String("String Based Problem");
+		
+		//Concatination
+		String str4= str1 + "\n" +str2 +"\n" + str3;
+		System.out.println(str4);
+	}
+}

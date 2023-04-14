2e16416e6bf446a101b6a4f3cb7cf90574147357
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-04-14 23:51:40 +0530

    Create PostiveNegative.java

---
+++ Java/Basic/PostiveNegative.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class PostiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0;
+		if(num>0) {
+			System.out.println("Postive");
+		}
+		else if(num<0) {
+			System.out.println("Negtive");
+		}
+		else {
+			System.out.println("Zero");
+		}
+	}
+
+}

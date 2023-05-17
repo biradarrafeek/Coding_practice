791cff8720afd0616f5e9b0538a7163086d5b1b0
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-05-17 12:10:18 +0530

    Create Find Area of Rectangle .java

---
+++ Interview/Find Area of Rectangle .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+        int l,b,area;
+        Scanner s = new Scanner(System.in);
+        l = s.nextInt();
+        b = s.nextInt();
+        area = l*b;
+        System.out.println(area);
+		
+	}
+}
